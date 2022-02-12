b64ed29c6784674ef284fa3b5b13f93e2305b9ea
Qbourgeois <72013160+Qbourgeois@users.noreply.github.com>
2022-02-12 21:13:07 +0100

    Level00 and carac

---
+++ New Game Project/Scenes/Level00/KinematicBody2D.gd
@@ -0,0 +1,23 @@
+extends KinematicBody2D
+
+const speed = 60
+const gravity = 10
+const Jump_power = -250
+const Floor = Vector2(0, -1)
+
+var velocity = Vector2()
+
+func _physics_process(delta):
+	
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = speed
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -speed
+	else:
+		velocity.x = 0
+	
+	if Input.is_action_pressed("ui_up"):
+		velocity.y = Jump_power
+		
+	velocity.y = velocity.y + gravity
+	velocity = move_and_slide(velocity, Floor)
