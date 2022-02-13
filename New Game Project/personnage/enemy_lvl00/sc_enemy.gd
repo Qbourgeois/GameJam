849760f510eec275fc7df74c3a5fcb0b0feae0b3
hassan zabatt <hassoune.liban77@outlook.com>
2022-02-13 22:10:16 +0100

    fix lvl 1

--- New Game Project/personnage/enemy_lvl00/sc_enemy.gd
+++ New Game Project/personnage/enemy_lvl00/sc_enemy.gd
@@ -7,20 +7,28 @@ const Floor = Vector2(0, -1)
 var velocity = Vector2()
 var dir = 1
 
+var isDead = false
+
 func _ready():
 	pass
+	
+func dead():
+	isDead = true
+	velocity = Vector2(0, 0)
+	queue_free()
 
 func _physics_process(delta):
-	velocity.x = Speed * dir 
+	if isDead == false:
+		velocity.x = Speed * dir 
 
-	if dir == 1:
-		$AnimatedSprite.flip_h = false
-	else:
-		$AnimatedSprite.flip_h = true
-	$AnimatedSprite.play("Walk")
-	velocity.y = 0
-	
-	velocity = move_and_slide(velocity, Floor)
+		if dir == 1:
+			$AnimatedSprite.flip_h = false
+		else:
+			$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play("Walk")
+		velocity.y = 0
+		
+		velocity = move_and_slide(velocity, Floor)
 	
 	if is_on_wall():
 		dir = dir * -1
