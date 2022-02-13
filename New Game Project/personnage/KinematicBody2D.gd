c11df8d9e1735aefa94899a19cc5346c87198271
Qbourgeois <72013160+Qbourgeois@users.noreply.github.com>
2022-02-13 02:51:08 +0100

    [FIX] and [ADD] level 00

--- New Game Project/personnage/KinematicBody2D.gd
+++ New Game Project/personnage/KinematicBody2D.gd
@@ -1,34 +1,44 @@
 extends KinematicBody2D
 
-const speed = 70
+const walk_speed = 70
+const run_speed = 140
 const gravity = 10
 const Jump_power = -450
 const Floor = Vector2(0, -1)
 
 var velocity = Vector2()
 var on_ground = false
+var jump_count = 0
+var moving_state = "walk"
+var speed = walk_speed
 
 func _physics_process(delta):
 	
+	if Input.is_action_pressed("ui_control"):
+		if on_ground:
+			moving_state = "run"
+		speed = run_speed
+	else:
+		if on_ground:
+			moving_state = "walk"
+		speed = walk_speed
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = speed
-		$AnimatedSprite.play("run")
+		$AnimatedSprite.play(moving_state)
 	elif Input.is_action_pressed("ui_left"):
-		$AnimatedSprite.play("run")
+		$AnimatedSprite.play(moving_state)
 		velocity.x = -speed
 	else:
 		velocity.x = 0
 		if on_ground == true:
-			$AnimatedSprite.play("idl")
-	
-	if Input.is_action_pressed("ui_up"):
-		if on_ground == true:
-			velocity.y = Jump_power
+			moving_state = "idl"
+			$AnimatedSprite.play(moving_state)
 	
 	velocity.y = velocity.y + gravity
 	
 	if is_on_floor():
 		on_ground = true
+		jump_count = 0
 	else:
 		on_ground = false
 	velocity = move_and_slide(velocity, Floor)
@@ -36,3 +46,10 @@ func _physics_process(delta):
 func _input(event):
 	if event.is_action_pressed("Inventory"):
 		Inventory.display()
+	if Input.is_action_pressed("ui_up"):
+		if on_ground:
+			jump_count += 1
+			velocity.y = Jump_power
+		elif jump_count < 1:
+			jump_count += 1
+			velocity.y = Jump_power
